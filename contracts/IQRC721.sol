ee9aec2d3710d8c494f75396990b8d64adbc4f85
zhengyi <zhengyi.bupt@qq.com>
2019-02-27 18:44:35 +0800

    fix

--- contracts/IQRC721.sol
+++ contracts/IQRC721.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.5.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/introspection/IERC165.sol";
-
-contract IQRC721 is IERC165 {
+contract IQRC721 {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
 
     function balanceOf(address owner) public view returns (uint256 balance);
     function ownerOf(uint256 tokenId) public view returns (address owner);
@@ -25,6 +29,10 @@ contract IQRC721 is IERC165 {
     function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256 tokenId);
 
     function tokenByIndex(uint256 index) public view returns (uint256);
+
+    function isMinter(address account) public view returns (bool);
+    function addMinter(address account) public;
+    function renounceMinter() public;
 
     function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public returns (bool);
 }
